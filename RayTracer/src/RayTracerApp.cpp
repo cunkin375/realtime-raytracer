884fb8eb41a5420875ee5fc6140cb6a899a8ac1e
ali <159713281+cunkin375@users.noreply.github.com>
2026-08-17 13:50:31 -0400

    add temporary lighting + sliders

--- RayTracer/src/RayTracerApp.cpp
+++ RayTracer/src/RayTracerApp.cpp
@@ -8,6 +8,24 @@
 
 #include "Util/Aliases.hpp"
 
+namespace
+{
+enum class RGB
+{
+    Red,
+    Green,
+    Blue,
+};
+
+enum class Direction
+{
+    X,
+    Y,
+    Z
+};
+
+} // namespace
+
 class ExampleLayer : public Walnut::Layer
 {
 private:
@@ -21,10 +39,40 @@ public:
     {
         ImGui::Begin("Settings");
         ImGui::Text("Last Render: %.3fms", last_render_time_);
-        if (ImGui::Button("Render"))
+
+        // if (ImGui::Button("Render"))
+        // {
+        //     Render();
+        // }
+
+        ImGui::Text("Sphere Colors:");
+        if (auto temp = renderer_.GetSphereColors().r; ImGui::SliderFloat("Red", &temp, 0.0f, 1.0f))
         {
-            Render();
+            UpdateColorValue(temp, RGB::Red);
         }
+        if (auto temp = renderer_.GetSphereColors().g; ImGui::SliderFloat("Green", &temp, 0.0f, 1.0f))
+        {
+            UpdateColorValue(temp, RGB::Green);
+        }
+        if (auto temp = renderer_.GetSphereColors().b; ImGui::SliderFloat("Blue", &temp, 0.0f, 1.0f))
+        {
+            UpdateColorValue(temp, RGB::Blue);
+        }
+
+        ImGui::Text("Light Direction:");
+        if (auto temp = renderer_.GetLightDirection().x; ImGui::SliderFloat("X", &temp, -1.0f, 1.0f))
+        {
+            UpdateLightDirection(temp, Direction::X);
+        }
+        if (auto temp = renderer_.GetLightDirection().y; ImGui::SliderFloat("Y", &temp, -1.0f, 1.0f))
+        {
+            UpdateLightDirection(temp, Direction::Y);
+        }
+        if (auto temp = renderer_.GetLightDirection().z; ImGui::SliderFloat("Z", &temp, -1.0f, 1.0f))
+        {
+            UpdateLightDirection(temp, Direction::Z);
+        }
+
         ImGui::End(); // Settings
 
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
@@ -37,7 +85,7 @@ public:
         if (image)
             ImGui::Image(image->GetDescriptorSet(),
                          { static_cast<f32>(image->GetWidth()), static_cast<f32>(image->GetHeight()) },
-                           ImVec2(0, 1), ImVec2(1, 0) );
+                         ImVec2(0, 1), ImVec2(1, 0));
 
         ImGui::End(); // Viewport
         ImGui::PopStyleVar();
@@ -53,6 +101,27 @@ public:
         renderer_.Render();
 
         last_render_time_ = timer.ElapsedMillis();
+    }
+
+private:
+    void UpdateColorValue(f32 new_value, RGB channel)
+    {
+        switch (channel)
+        {
+            case RGB::Red: renderer_.SetRed(new_value); break;
+            case RGB::Green: renderer_.SetGreen(new_value); break;
+            case RGB::Blue: renderer_.SetBlue(new_value); break;
+        }
+    }
+
+    void UpdateLightDirection(f32 new_value, Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction::X: renderer_.SetX(new_value); break;
+            case Direction::Y: renderer_.SetY(new_value); break;
+            case Direction::Z: renderer_.SetZ(new_value); break;
+        }
     }
 };
 
--- RayTracer/src/Renderer.cpp
+++ RayTracer/src/Renderer.cpp
@@ -36,26 +36,29 @@ void Renderer::Render()
 
     for (auto thread{ 0zu }; thread < thread_count_; ++thread)
     {
-        threads_.emplace_back([&, thread](){
-            for (auto y{ thread }; y < final_image_->GetHeight(); y += thread_count_)
+        threads_.emplace_back(
+            [&, thread]()
             {
-                for (auto x{ 0zu }; x < final_image_->GetWidth(); ++x)
+                for (auto y{ thread }; y < final_image_->GetHeight(); y += thread_count_)
                 {
-                    auto pixel_coordinate =
-                        fVector2{ static_cast<f32>(x) / static_cast<f32>(final_image_->GetWidth()),
-                                static_cast<f32>(y) / static_cast<f32>(final_image_->GetHeight()) };
+                    for (auto x{ 0zu }; x < final_image_->GetWidth(); ++x)
+                    {
+                        auto pixel_coordinate =
+                            fVector2{ static_cast<f32>(x) / static_cast<f32>(final_image_->GetWidth()),
+                                      static_cast<f32>(y) / static_cast<f32>(final_image_->GetHeight()) };
 
-                    pixel_coordinate = pixel_coordinate * 2.f - 1.f;
+                        pixel_coordinate = pixel_coordinate * 2.f - 1.f;
 
-                    pixel_coordinate.x *= aspect_ratio;
+                        pixel_coordinate.x *= aspect_ratio;
 
-                    fVector4 pixel_color = PerPixel(pixel_coordinate);
-                    pixel_color.Clamp(fVector4{ 0.0f }, fVector4{ 1.0f });
+                        fVector4 pixel_color = PerPixel(pixel_coordinate);
+                        pixel_color.Clamp(fVector4{ 0.0f }, fVector4{ 1.0f });
 
-                    image_data_[x + y * final_image_->GetWidth()] = ImageColor::ConvertToRGBA(pixel_color);
+                        image_data_[x + y * final_image_->GetWidth()] =
+                            ImageColor::ConvertToRGBA(pixel_color);
+                    }
                 }
-            }
-        });
+            });
     }
 
     threads_.clear();
@@ -69,8 +72,8 @@ fVector4 Renderer::PerPixel(fVector2 coord)
 {
     f32 radius = 0.5f;
 
-    auto ray_origin = fVector3{ 0.f, 0.f, 2.f };
-    auto ray_direction = fVector3{ coord.x, coord.y, -1.f };
+    auto ray_origin = fVector3{ 0, 0, 1.5f };
+    auto ray_direction = fVector3{ coord.x, coord.y, -1 };
 
     f32 a = ray_direction.DotProduct(ray_direction);
     f32 b = 2.f * ray_origin.DotProduct(ray_direction);
@@ -84,14 +87,23 @@ fVector4 Renderer::PerPixel(fVector2 coord)
         // u32 color = Math::Rand::GenerateRandomNumber<u32>();
         // return fVector4{ static_cast<f32>(color & 0xFF), static_cast<f32>((color >> 8) & 0xFF),
         //                  static_cast<f32>((color >> 16) & 0xFF), 255.f };
-        return fVector4{0.f, 0.f, 0.f, 0.f};
+        return fVector4{ 0.f, 0.f, 0.f, 0.f };
     }
 
     float t[] = { (-b - std::sqrt(discriminant)) / (2.f * a), (-b + std::sqrt(discriminant)) / (2.f * a) };
 
     fVector3 hit_position = ray_origin + ray_direction * t[0];
-    fColor normal = hit_position;
-    normal.Normalize();
+    fColor normal = fVector3::Normalize(hit_position);
 
-    return fVector4{ normal.r, normal.g, normal.b, static_cast<f32>(0xFF) };
+    // auto light_direction = fVector3::Normalize(fVector3{-1, -1, -1});
+
+    auto light_direction = fVector3::Normalize(light_direction_);
+
+    f32 intensity = std::max(fVector3::DotProduct(normal, -light_direction), 0.0f);
+
+    // auto sphere_color = normal * 0.5 + 0.5;
+
+    auto sphere_color = sphere_colors_ * intensity;
+
+    return fVector4{ sphere_color.r, sphere_color.g, sphere_color.b, static_cast<f32>(0xFF) };
 }
