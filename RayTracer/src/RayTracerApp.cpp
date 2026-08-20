7c4ab670773736f44997acb883d28b68cf9702b6
ali <159713281+cunkin375@users.noreply.github.com>
2026-08-20 11:39:54 -0400

    add triggers for Renderer::ResetFrameIndex() on UI updates

--- RayTracer/src/RayTracerApp.cpp
+++ RayTracer/src/RayTracerApp.cpp
@@ -51,68 +51,73 @@ public:
     RayTracerLayer() : camera_(FOV{ 45.0f }, NearPlane{ 0.1f }, FarPlane{ 100.0f })
     {
         scene_.materials.push_back({ .albedo{ 1.f, 0.f, 1.f }, .roughness = 0.15f });
-        scene_.materials.push_back({ .albedo{ 0.f, 0.1f, 0.9f }, .roughness = 0.05f });
+        scene_.materials.push_back({ .albedo{ 1.f, 1.0f, 0.0f }, .roughness = 0.05f });
 
         scene_.spheres.push_back({ .position{ 0.f, -101.f, 0.f }, .radius = 100.f, .material_index = 0 });
         scene_.spheres.push_back({ .position{ 0.f }, .radius = 1.0f, .material_index = 1 });
     }
 
-    virtual void OnUpdate(f32 ts) override { camera_.OnUpdate(ts); }
+    virtual void OnUpdate(f32 timestamp) override
+    {
+        if (camera_.OnUpdate(timestamp))
+            renderer_.ResetFrameIndex();
+    }
 
     virtual void OnUIRender() override
     {
+        bool reset_frame_index{ false };
+
         ImGui::Dummy(ImVec2{ 0.f, 10.f });
 
-        ImGui::Begin("Settings");
+        ImGui::Begin("Scene");
         ImGui::Text("Last Render: %.3fms", last_render_time_);
 
         if (ImGui::Button("Add Sphere"))
         {
             AddSphere();
+            reset_frame_index = true;
         }
 
         MyGui::Padding(ImVec2{ 0.f, 10.f });
 
+        // Spheres
         for (auto [id, sphere] : scene_.spheres | std::views::enumerate)
         {
             ImGui::PushID(id);
 
-            ImGui::DragFloat3("Position", Math::ValuePointer(sphere.position), 0.01f);
-            ImGui::DragFloat("Radius", &sphere.radius, 0.1f);
-            ImGui::DragInt("Material", &sphere.material_index, 1, 0,
-                           static_cast<int>(scene_.materials.size() - 1));
+            if (ImGui::DragFloat3("Position", Math::ValuePointer(sphere.position), 0.01f)
+                || ImGui::DragFloat("Radius", &sphere.radius, 0.1f)
+                || ImGui::DragInt("Material", &sphere.material_index, 1, 0,
+                                  static_cast<int>(scene_.materials.size() - 1)))
+            {
+                reset_frame_index = true;
+            }
 
             MyGui::Padding(ImVec2{ 0.f, 10.f });
 
             ImGui::PopID();
         }
 
+        // Materials
         for (auto [id, material] : scene_.materials | std::views::enumerate)
         {
             ImGui::PushID(id);
 
-            ImGui::ColorEdit3("Albedo", Math::ValuePointer(material.albedo));
-            ImGui::DragFloat("Roughness", &material.roughness, 0.01f, 0.f, 1.f);
-            ImGui::DragFloat("Metallic", &material.metallic, 0.01f, 0.f, 1.f);
+            if (ImGui::ColorEdit3("Albedo", Math::ValuePointer(material.albedo))
+                || ImGui::DragFloat("Roughness", &material.roughness, 0.01f, 0.f, 1.f)
+                || ImGui::DragFloat("Metallic", &material.metallic, 0.01f, 0.f, 1.f))
+            {
+                reset_frame_index = true;
+            }
 
             ImGui::PopID();
         }
 
-        ImGui::Text("Light Direction:");
-        if (auto temp = renderer_.GetLightDirection().x; ImGui::SliderFloat("X", &temp, -1.f, 1.f))
-        {
-            UpdateLightDirection(temp, Direction::X);
-        }
-        if (auto temp = renderer_.GetLightDirection().y; ImGui::SliderFloat("Y", &temp, -1.f, 1.f))
-        {
-            UpdateLightDirection(temp, Direction::Y);
-        }
-        if (auto temp = renderer_.GetLightDirection().z; ImGui::SliderFloat("Z", &temp, -1.f, 1.f))
-        {
-            UpdateLightDirection(temp, Direction::Z);
-        }
+        MyGui::Padding(ImVec2{ 0.f, 10.f });
+        if (ImGui::DragFloat3("Light Direction", Math::ValuePointer(scene_.light_direction), 0.01f, -1.f, 1.f))
+            reset_frame_index = true;
 
-        ImGui::End(); // Settings
+        ImGui::End(); // Scene
 
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
         ImGui::Begin("Viewport");
@@ -122,12 +127,24 @@ public:
 
         auto image = renderer_.GetFinalImage();
         if (image)
+        {
             ImGui::Image(image->GetDescriptorSet(),
                          { static_cast<f32>(image->GetWidth()), static_cast<f32>(image->GetHeight()) },
                          ImVec2(0, 1), ImVec2(1, 0));
+        }
 
         ImGui::End(); // Viewport
         ImGui::PopStyleVar();
+
+        ImGui::Begin("Settings");
+        ImGui::Checkbox("Accumulate", &renderer_.GetSettings().accumulate);
+        if (ImGui::Button("Reset"))
+            renderer_.ResetFrameIndex();
+        ImGui::End(); // Settings
+
+        // NOTE: keep UI above this code
+        if (reset_frame_index)
+            renderer_.ResetFrameIndex();
 
         Render();
     }
@@ -144,16 +161,6 @@ public:
     }
 
 private:
-    void UpdateLightDirection(f32 new_value, Direction direction)
-    {
-        switch (direction)
-        {
-            case Direction::X: renderer_.SetX(new_value); break;
-            case Direction::Y: renderer_.SetY(new_value); break;
-            case Direction::Z: renderer_.SetZ(new_value); break;
-        }
-    }
-
     void AddSphere() { scene_.spheres.push_back({ .position{ 0.f }, .radius = 1.0f, .material_index = 0 }); }
 };
 
